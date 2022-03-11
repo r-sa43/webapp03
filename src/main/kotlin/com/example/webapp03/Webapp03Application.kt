32f56e9fcbbcf6c4d54b85bdb5ccb217f6ab0cf2
RY4121 <yoshida.rariru@gmail.com>
2022-03-11 13:27:59 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/webapp03/Webapp03Application.kt
@@ -0,0 +1,11 @@
+package com.example.webapp03
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Webapp03Application
+
+fun main(args: Array<String>) {
+	runApplication<Webapp03Application>(*args)
+}
---
+++ src/test/kotlin/com/example/webapp03/Webapp03ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.webapp03
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Webapp03ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
