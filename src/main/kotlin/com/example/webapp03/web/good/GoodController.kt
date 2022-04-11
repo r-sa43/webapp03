7a7ae5e3ff70f9b5fe575c6a3376cd794a1c1110
RY4121 <yoshida.rariru@gmail.com>
2022-04-11 17:04:47 +0900

    add error handling for registering duplicate good

--- src/main/kotlin/com/example/webapp03/web/good/GoodController.kt
+++ src/main/kotlin/com/example/webapp03/web/good/GoodController.kt
@@ -1,21 +1,38 @@
 package com.example.webapp03.web.good
 
 import com.example.webapp03.domain.good.GoodService
+import org.springframework.dao.DuplicateKeyException
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequestMapping("good")
 class GoodController(
-    private val goodService: GoodService
+    private val goodService: GoodService,
 ) {
 
     @PutMapping("")
     fun updateGoodCount(
-        @RequestBody goodInfo: GoodInfo
+        @RequestBody goodInfo: GoodInfo,
+        result: BindingResult,
+        redirectAttributes: RedirectAttributes,
+        loginUser: Authentication,
     ): String {
+
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errors", result)
+            redirectAttributes.addFlashAttribute("request", goodInfo)
+
+            return "post/list"
+        }
+
         if (goodInfo.marked) {
             goodService.countDownGoodCnt(goodInfo.postId)
         } else {
@@ -23,5 +40,16 @@ class GoodController(
         }
 
         return "post/list"
+    }
+
+    @ExceptionHandler(DuplicateKeyException::class)
+    fun duplicateKeyExceptionHandler(
+        ex: DuplicateKeyException,
+        model: Model
+    ): String {
+        model.addAttribute("error", "Internal Server Error")
+        model.addAttribute("message", "DuplicateKeyException")
+
+        return "error"
     }
 }
